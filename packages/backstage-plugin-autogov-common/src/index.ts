91d9c700b5b73f7d8008ec94a1b24bfba8df3915
Amber Beasley <amber.beasley@liatrio.com>
2024-11-08 16:47:11 -0500

    fix: move constants to processor

--- packages/backstage-plugin-autogov-common/src/index.ts
+++
@@ -1,3 +0,0 @@
-export * from "./plugin";
-export * from "./constants";
-export * from "./utils";
--- packages/backstage-plugin-autogov-common/src/plugin.test.ts
+++
@@ -1,7 +0,0 @@
-import { autogovCommonPlugin } from "./plugin";
-
-describe("autogov-common", () => {
-  it("should export plugin", () => {
-    expect(autogovCommonPlugin).toBeDefined();
-  });
-});
