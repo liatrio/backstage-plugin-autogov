761099c46b44361abbb7c11306853c2a8bdde641
Daniel Hagen <5039466+dbhagen@users.noreply.github.com>
2024-11-08 12:48:59 -0600

    feat: add autogov processor

---
+++ packages/backstage-plugin-backend-module-autogov-processor/README.md
@@ -0,0 +1,5 @@
+# @liatrio/backstage-plugin-backend-module-autogov-processor
+
+The autogov-processor backend module for the catalog plugin.
+
+This plugin was created through the Backstage CLI
---
+++ packages/backstage-plugin-backend-module-autogov-processor/src/index.ts
@@ -0,0 +1,8 @@
+/***/
+/**
+ * The autogov-processor backend module for the catalog plugin.
+ *
+ * @packageDocumentation
+ */
+
+export { catalogModuleAutogovProcessor as default } from "./module";
