2dbb8ac0965a56f1639da4f2876406bf92ecc2aa
amber-beasley-liatrio <132274249+amber-beasley-liatrio@users.noreply.github.com>
2024-11-08 20:19:56 -0500

    feat: add release backend (#15)
    * feat: add release backend

---
+++ packages/backstage-plugin-autogov-releases-backend/src/plugin.ts
@@ -0,0 +1,43 @@
+import {
+  coreServices,
+  createBackendPlugin,
+} from "@backstage/backend-plugin-api";
+import { createRouter } from "./service/router";
+
+/**
+ * autogovReleasesPlugin backend plugin
+ *
+ * @public
+ */
+export const autogovReleasesPlugin = createBackendPlugin({
+  pluginId: "autogov-releases",
+  register(env) {
+    env.registerInit({
+      deps: {
+        config: coreServices.rootConfig,
+        logger: coreServices.logger,
+        auth: coreServices.auth,
+        discovery: coreServices.discovery,
+        httpRouter: coreServices.httpRouter,
+      },
+      async init({ config, logger, auth, discovery, httpRouter }) {
+        httpRouter.use(
+          await createRouter({
+            config,
+            logger,
+            auth,
+            discovery,
+          }),
+        );
+        httpRouter.addAuthPolicy({
+          path: "/health",
+          allow: "unauthenticated",
+        });
+        httpRouter.addAuthPolicy({
+          path: "/releases",
+          allow: "unauthenticated",
+        });
+      },
+    });
+  },
+});
